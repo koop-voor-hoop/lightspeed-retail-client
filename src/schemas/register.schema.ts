8c890d7e44b6a61e1f0a1266b255222319ce1dff
kalucky0 <kalucki23@gmail.com>
2026-05-30 21:53:14 +0200

    Allow empty-string ints in register schema

--- src/schemas/register.schema.ts
+++ src/schemas/register.schema.ts
@@ -2,6 +2,11 @@ import z from 'zod';
 import { attributesSchema } from './attributes.schema';
 import { booleanLikeSchema, integerLikeSchema, oneOrMany } from './helper.schema';
 
+const optionalIntegerLikeSchema = z.preprocess(
+  (value) => (value === '' ? undefined : value),
+  integerLikeSchema.optional(),
+);
+
 export const registerSchema = z.object({
   registerID: integerLikeSchema,
   name: z.string().optional(),
@@ -9,9 +14,9 @@ export const registerSchema = z.object({
   openTime: z.string().optional(),
   archived: booleanLikeSchema.optional(),
   tipEnabled: booleanLikeSchema.optional(),
-  openEmployeeID: integerLikeSchema.optional(),
-  shopID: integerLikeSchema.optional(),
-  ccTerminalID: integerLikeSchema.optional(),
+  openEmployeeID: optionalIntegerLikeSchema,
+  shopID: optionalIntegerLikeSchema,
+  ccTerminalID: optionalIntegerLikeSchema,
 });
 
 export const registerResponseSchema = z.object({
